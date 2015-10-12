98c0d58700cb58c0034137c994439af1cf904698
Phui Yee Lim <joanna_yee@outlook.com>
2015-10-12 09:25:35 -0400

    another try

---
+++ Dog.java
@@ -0,0 +1,17 @@
+public class Dog {
+	private int age;
+	private String breed;
+	private String name;
+
+	public Dog(){
+
+	}
+	public int getAge(){
+		return age;
+	}
+
+	public void setAge(int age){
+		this.age=age;
+
+	}
+}
